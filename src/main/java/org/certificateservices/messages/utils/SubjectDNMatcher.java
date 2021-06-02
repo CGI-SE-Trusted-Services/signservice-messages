4196bf548bca38d99f7a252529975db18439d7d0
Philip Vendil <p.vendil@cgi.com>
2021-06-02 12:51:45 +0200

    CS.2773 fixed PR comments.

--- src/main/java/org/certificateservices/messages/utils/SubjectDNMatcher.java
+++ src/main/java/org/certificateservices/messages/utils/SubjectDNMatcher.java
@@ -1,3 +1,15 @@
+/************************************************************************
+ *                                                                       *
+ *  Certificate Service - Messages                                       *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Affero General Public License   *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 3   of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.certificateservices.messages.utils;
 
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
