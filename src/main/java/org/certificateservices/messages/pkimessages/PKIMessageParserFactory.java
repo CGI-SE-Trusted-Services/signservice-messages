ca0fb48c41ba978b848bd7c2b4760d6bcb90c7be
Philip Vendil <p.vendil@cgi.com>
2022-05-02 13:31:07 +0200

    CS-3711 update licens header in files

--- src/main/java/org/certificateservices/messages/pkimessages/PKIMessageParserFactory.java
+++ src/main/java/org/certificateservices/messages/pkimessages/PKIMessageParserFactory.java
@@ -3,7 +3,7 @@
 *  Certificate Service - PKI Messages                                   *
 *                                                                       *
 *  This software is free software; you can redistribute it and/or       *
-*  modify it under the terms of the GNU Affero General Public License   *
+*  modify it under the terms of the GNU Lesser General Public License   *
 *  License as published by the Free Software Foundation; either         *
 *  version 3   of the License, or any later version.                    *
 *                                                                       *
