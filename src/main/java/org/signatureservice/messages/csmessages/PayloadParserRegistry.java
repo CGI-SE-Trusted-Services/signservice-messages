c221e533d5c74ee5721e8c9a49922b8268772313
fredrik.wickstrom <fredrik.wickstrom@cgi.com>
2025-03-12 10:58:34 +0100

    EIDDSS-1075 remove package org.signatureservice.messages.signrequest

--- src/main/java/org/signatureservice/messages/csmessages/PayloadParserRegistry.java
+++ src/main/java/org/signatureservice/messages/csmessages/PayloadParserRegistry.java
@@ -19,7 +19,6 @@ import java.util.Set;
 import org.signatureservice.messages.MessageProcessingException;
 import org.signatureservice.messages.assertion.AssertionPayloadParser;
 import org.signatureservice.messages.credmanagement.CredManagementPayloadParser;
-import org.signatureservice.messages.signrequest.SignRequestPayloadParser;
 import org.signatureservice.messages.sysconfig.SysConfigPayloadParser;
 
 /**
@@ -56,7 +55,6 @@ public class PayloadParserRegistry {
 		  payloadParserRegistry.put(SysConfigPayloadParser.NAMESPACE, SysConfigPayloadParser.class);
 		  payloadParserRegistry.put(CredManagementPayloadParser.NAMESPACE, CredManagementPayloadParser.class);
 		  payloadParserRegistry.put(AssertionPayloadParser.NAMESPACE, AssertionPayloadParser.class);
-		  payloadParserRegistry.put(SignRequestPayloadParser.NAMESPACE, SignRequestPayloadParser.class);
 		}
 	}
 	
